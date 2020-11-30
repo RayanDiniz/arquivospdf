17e00fae23c7979c4247a065a9ce9bfb05dbb3bd
RayanDiniz <rayancassiokiol@gmail.com>
2020-11-30 10:03:42 -0300

    corrigi

--- src/upload/upload.php
+++ src/upload/upload.php
@@ -1,5 +1,5 @@
 <?php
-include('http://nuvem.space/src/login/config.php');
+include('../login/config.php');
 //include('../login/local.php');
 include('../login/verifica_login.php');
 include('../login/redirect.php');
@@ -26,7 +26,7 @@ if (isset($_FILES['arquivo'])) {
 
         $sql_code = "INSERT INTO arquivos (titulo, link, tipo, cpf_cliente, data) VALUES ('$titulo', '$link', '$tipo', '$cpf', '$data')";
 
-        if ($con->query($sql_code)){
+        if ($conexao_pdo->query($sql_code)){
             move_uploaded_file($_FILES['arquivo']['tmp_name'], $diretorio . $link); //efetua o upload
             $msg = "Arquivo enviado com sucesso!";
         }else{
