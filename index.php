26bb3aaef5325b207db89e1145a91e2debaef851
RayanDiniz <rayancassiokiol@gmail.com>
2020-12-22 15:09:40 -0300

    report

--- index.php
+++ index.php
@@ -18,9 +18,8 @@ include('src/login/redirect.php');
     <link rel="stylesheet" href="src/style.css">
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css"
-    integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
 </head>
 
 <body>
@@ -35,29 +34,29 @@ include('src/login/redirect.php');
                     <i class="fas fa-home" aria-hidden="true"></i>
                     <span>Inicio</span>
                 </a>
-				
+
                 <a href="src/cria-clientes/" target="Post" class="list-group-item list-group-item-action bg-light">
                     <i class="fas fa-users" aria-hidden="true"></i>
                     <span>Clientes</span>
                 </a>
-                
-                <?php if ($_SESSION['master'] === 'adm') {?>
-                                <a href="src/cria-usuarios/" target="Post" class="list-group-item list-group-item-action bg-light">
-                                    <i class="fas fa-user-tie" aria-hidden="true"></i>
-                                    <span>Usuários do Sistema</span>
-                                </a>
-                <?php	}; ?>
+
+                <?php if ($_SESSION['master'] === 'adm') { ?>
+                    <a href="src/cria-usuarios/" target="Post" class="list-group-item list-group-item-action bg-light">
+                        <i class="fas fa-user-tie" aria-hidden="true"></i>
+                        <span>Usuários do Sistema</span>
+                    </a>
+                <?php    }; ?>
 
                 <a href="src/upload" target="Post" class="list-group-item list-group-item-action bg-light">
                     <i class="fas fa-cloud-upload-alt" aria-hidden="true"></i>
                     <span>Enviar Arquivo</span>
-				</a>
+                </a>
 
                 <a href="https://www.ilovepdf.com/pt/word_para_pdf" target="Post" class="list-group-item list-group-item-action bg-light">
                     <i class="fas fa-sync" aria-hidden="true"></i>
                     <span>Converter Arquivo</span>
                 </a>
-                
+
                 <a href="src/login/sair.php" class="list-group-item list-group-item-action bg-light">
                     <i class="fas fa-sign-out-alt" aria-hidden="true"></i>
                     <span>Sair</span>
@@ -73,25 +72,22 @@ include('src/login/redirect.php');
                 <a href="#" id="menu-toggle">
                     <i class="fa fa-arrow-left" aria-hidden="true"></i>
                 </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse"
-                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                    aria-label="Toggle navigation">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                
+
                     <ul class="navbar-nav ml-auto">
-                            
+
                         <li class="nav-item">
                             Busca:&nbsp;&nbsp;
-                        </li>     
+                        </li>
                         <li class="nav-item">
 
                             <form name="frmBusca" method="post" target="Post" action="src/pesquisa/cliente.php?a=buscar">
                                 <div class="input-group">
-                                    <input name="cliente" id="txt_consulta" placeholder="pelo nome do cliente" type="text"
-                                        class="form-control" required>
+                                    <input name="cliente" id="txt_consulta" placeholder="pelo nome do cliente" type="text" class="form-control" required>
                                     <div class="input-group-append">
                                         <button class="btn btn-primary" type="submit">
                                             <i class="fas fa-search" aria-hidden="true"></i>
@@ -103,13 +99,12 @@ include('src/login/redirect.php');
                         </li>
                         <li class="nav-item">
                             &nbsp;&nbsp;ou&nbsp;&nbsp;
-                        </li>     
+                        </li>
                         <li class="nav-item">
 
                             <form name="frmBusca" method="post" target="Post" action="src/pesquisa/cpf.php?a=buscar">
                                 <div class="input-group">
-                                    <input name="cpf" id="txt_consulta" placeholder="pelo CPF do cliente" type="text" oninput="mascara(this)"
-                                        class="form-control" required>
+                                    <input name="cpf" id="txt_consulta" placeholder="pelo CPF do cliente" type="text" oninput="mascara(this)" class="form-control" required>
                                     <div class="input-group-append">
                                         <button class="btn btn-success" type="submit">
                                             <i class="fas fa-search" aria-hidden="true"></i>
@@ -121,7 +116,7 @@ include('src/login/redirect.php');
                         </li>
                         <li class="nav-item">
                             &nbsp;&nbsp;ou&nbsp;&nbsp;
-                        </li>   
+                        </li>
                         <li class="nav-item">
 
                             <form name="frmBusca" method="post" target="Post" action="src/pesquisa/arquivo.php?a=buscar">
@@ -140,16 +135,16 @@ include('src/login/redirect.php');
                 </div>
             </nav>
 
-            <div class="container-fluid"    style="height:80%;">
+            <div class="container-fluid" style="height:80%;">
                 <h3 class="mt-4"><b><?php echo $_SESSION['nome_usuario'] ?></b></h3>
 
-                <iframe name="Post" src="info.html" width="100%" height="100%" frameborder=0 scrolling=yes>
+                <iframe name="Post" src="info.php" width="100%" height="100%" frameborder=0 scrolling=yes>
 
                 </iframe>
             </div>
-			<div class=" text-center text-muted" style="margin-bottom:0">
-				<p>Desenvolvido por RayanDiniz &copy;</p>
-			</div>
+            <div class=" text-center text-muted" style="margin-bottom:0">
+                <p>Desenvolvido por RayanDiniz &copy;</p>
+            </div>
         </div>
     </div>
     <!-- /#page-content-wrapper -->
@@ -163,7 +158,7 @@ include('src/login/redirect.php');
     <script src="http://nuvem.space/src/app.js"></script>
     <!-- Menu Toggle Script -->
     <script>
-        $("#menu-toggle").click(function (e) {
+        $("#menu-toggle").click(function(e) {
             e.preventDefault();
             $("#wrapper").toggleClass("toggled");
         });
