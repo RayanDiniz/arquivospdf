919497d4d6ec2b20f6b53b4a021553766637a5f8
RayanDiniz <rayancassiokiol@gmail.com>
2020-12-22 15:14:12 -0300

    report

--- send-mail.php
+++ send-mail.php
@@ -1,11 +1,16 @@
 <?php
-
+include('src/login/config.php');
+//include('src/login/local.php');
 include('src/login/verifica_login.php');
+include('src/login/redirect.php');
 
   $nome =  $_SESSION['nome_usuario'];
   $email= $_POST['email'];
   $mensagem= $_POST['mensagem'];
   $to = "rayancassiokiol@gmail.com";
-  $assunto = "Mensagem de ".$email;
+  $assunto = "Mensagem de ".$nome." do e-mail: ".$email;
   mail($to,$assunto,$mensagem);
+  
+// Redireciona para o info.php
+header('location: ./info.php');
 ?>
