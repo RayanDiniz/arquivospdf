25e7d93202f6b2b524e88093e83eb35a448f4e97
RayanDiniz <rayancassiokiol@gmail.com>
2020-12-22 15:44:41 -0300

    tamanho bemvindo

--- info.php
+++ info.php
@@ -11,7 +11,7 @@
 </head>
 
 <body>
-  <div class="container-flex">
+  <div class="container">
     <div class="jumbotron">
       <h3 class="display-5">Bem-Vindo(a)</h3>
       <p class="lead">ao sistema de armazenamento de arquivos pdf na nuvem</p>
