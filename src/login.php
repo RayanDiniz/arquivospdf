b0f9b68f94b8e9c28bee7fb822f07902d55c3282
RayanDiniz <rayancassiokiol@gmail.com>
2021-01-13 00:24:30 -0300

    sistema de pagamento

--- src/login.php
+++ src/login.php
@@ -39,16 +39,16 @@ session_start();
 					$pdo_verifica->execute();
 					
 					while ($fetch = $pdo_verifica->fetch()) {
-						$data1 = $fetch['data'];
-						$data1 = implode("/", array_reverse(explode("-", $data1)));
-						$data2 = date ('d/m/Y');
+						$data2 = $fetch['data'];
+						$data2 = implode("/", array_reverse(explode("-", $data1)));
+						$data1 = date ('d/m/Y');
 						// Comparando as Datas
 						if (strtotime($data1) > strtotime($data2)) {
-							echo '<div class="alert alert-info" role="alert"><strong>Atenção!</strong> Seu boleto está disponível para pagamento e vence no dia '.$data1.'.</div>';
+							echo '<div class="alert alert-info" role="alert"><strong>Atenção!</strong> Seu boleto está disponível para pagamento e vence no dia '.$data2.'.</div>';
 						}elseif (strtotime($data1) == strtotime($data2)) {
 							echo '<div class="alert alert-warning" role="alert"><strong>Atenção!</strong> Seu boleto Vence hoje.</div>';
 						}else{
-							echo '<div class="alert alert-danger" role="alert"><strong>Atenção!</strong> Seu boleto Venceu em '.$data1.', entre em contato com o desenvolvedor.</div>';
+							echo '<div class="alert alert-danger" role="alert"><strong>Atenção!</strong> Seu boleto Venceu em '.$data2.', entre em contato com o desenvolvedor.</div>';
 						}
 					}
 				?>
