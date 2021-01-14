94981ea8267852b83ec6775ca48544243ea0bed4
RayanDiniz <rayancassiokiol@gmail.com>
2021-01-14 14:48:49 -0300

    style layout

--- src/login.php
+++ src/login.php
@@ -5,54 +5,59 @@ session_start();
 
 include('./components/Header-html.php');
 ?>
-	<div class="container-fluid bg-light justify-content-center align-items-center">
-		<div class="row">
-			<br><br><br>
-		</div>
-		<div class="row ">
-			<div class="col-6 text-center" style="color: #00BFFF">
-				<h1>Nuvem</h1>
-				<!--<i class="fas fa-angle-double-right fa-2x"></i>-->
-				<i class="fa fa-cloud fa-5x" aria-hidden="true"></i>
+<div class="container-fluid bg-light pt-4">
+	<div class="container p-5">
+		<div class="row pt-5">
+			<div class="col">
+				<h1 style="color: #00BFFF">
+					<!--<i class="fas fa-angle-double-right fa-2x"></i>-->
+					<i class="fa fa-cloud fa-1x" aria-hidden="true"></i>
+					Nuvem
+				</h1>
+				<h4 class="text-secondary">A Nuvem.space ajuda você armazenar com segurança os documentos de sua
+					empresa com facilidade.
+				</h4>
 			</div>
-			<div class="col-4 shadow p-3 mb-5 bg-white rounded">
+			<div class="col shadow p-3 mb-5 bg-white rounded-lg">
 				<?php echo $avisoPagamentoLogin ?>
 				<form action="../" method="post">
 					<?php if (!empty($_SESSION['login_erro'])) :
 						echo "<div class='alert alert-danger'>" . $_SESSION['login_erro'] . "</div>";
 						$_SESSION['login_erro'] = '';
 					endif; ?>
-					<div class=" input-group">
-						<div class="input-group-prepend">
-							<span class="input-group-text"><i class="fa fa-user" aria-hidden="true"></i></span>
+					<div class=" input-group input-group-lg">
+							<div class="input-group-prepend">
+								<span class="input-group-text"><i class="fa fa-user" aria-hidden="true"></i></span>
+							</div>
+							<input type="text" name="usuario" id="" class="form-control" placeholder="Nome do Usuário"
+								aria-describedby="helpId" required disabled />
 						</div>
-						<input type="text" name="usuario" id="" class="form-control" placeholder="Nome do Usuario" aria-describedby="helpId" required>
-					</div>
-					<small id="helpId" class="text-muted">Digite o nome de seu usuario para logar na nuvem.</small>
 
-					<div class="input-group">
-						<div class="input-group-prepend">
-							<span class="input-group-text"><i class="fa fa-key" aria-hidden="true"></i></span>
+						<div class=" input-group input-group-lg mt-3">
+							<div class="input-group-prepend">
+								<span class="input-group-text"><i class="fa fa-key" aria-hidden="true"></i></span>
+							</div>
+							<input type="password" name="senha" id="" class="form-control"
+								placeholder="Senha do Usuário" aria-describedby="helpId" required disabled />
 						</div>
-						<input type="password" name="senha" id="" class="form-control" placeholder="Senha do Usuario" aria-describedby="helpId" required>
-					</div>
-					<small id="helpId" class="text-muted">Digite a senha de seu usuario para logar na nuvem.</small>
-					<br />
-					<div class="text-center">
-						<button type="sumit" class="btn btn-primary btn-lg text-center">Entrar</button>
-					</div>
-					<br />
+
+						<button type="sumit" class="btn btn-primary btn-lg btn-block mt-3">Entrar</button>
+
+						<hr />
+						<small id="helpId" class="text-muted">Digite o nome de seu usuário e a senha para logar na
+							nuvem.</small>
+						<small id="helpId" class="text-muted">Caso tenha esquecido sua senha entre em contato com o <a
+								href="https://linkwhats.app/8a025b">Desenvolvedor</a></small>
 				</form>
-			</div>
-			<div class="col-2">
 			</div>
 		</div>
 		<div class="row">
-			<div class=" text-center text-muted" style="margin-bottom:0">
+			<div class="col text-center text-muted mb-0">
 				<p>Desenvolvido por RayanDiniz &copy;</p>
 			</div>
 		</div>
 	</div>
+</div>
 	<!-- Optional JavaScript -->
 	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
