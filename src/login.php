4fcd3f37092071683e33d572444a3e1f4982674a
RayanDiniz <rayancassiokiol@gmail.com>
2021-01-14 11:49:39 -0300

    configuranção de variavéis de pagamentos

--- src/login.php
+++ src/login.php
@@ -2,30 +2,9 @@
 include('./variavel-aviso.php');
 // Inicia a sessão
 session_start();
+
+include('../components/Header-html.php');
 ?>
-
-<!doctype html>
-<html lang="pt-br">
-
-<head>
-	<title>Login</title>
-	<!-- Required meta tags -->
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-	<!-- Bootstrap CSS -->
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
-	<style type="text/css">
-		html,
-		body,
-		.row,
-		.container {
-			height: 100%;
-		}
-	</style>
-</head>
-
 <body>
 	<div class="container">
 		<div class="row justify-content-center align-items-center">
