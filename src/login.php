590eca6f5d019a41740b17c48315f1c45beee434
RayanDiniz <rayancassiokiol@gmail.com>
2021-01-14 11:15:04 -0300

    configuranção de variavéis de pagamentos

--- src/login.php
+++ src/login.php
@@ -1,5 +1,5 @@
 ﻿<?php
-include('./login/config.php');
+include('./variavel-aviso.php');
 // Inicia a sessão
 session_start();
 ?>
@@ -34,26 +34,7 @@ session_start();
 					<!--<i class="fas fa-angle-double-right fa-2x"></i>-->
 					<i class="fa fa-cloud fa-5x" aria-hidden="true"></i>
 				</div>
-				<?php
-					$pdo_verifica = $conexao_pdo->prepare('SELECT * FROM pagamentos WHERE status = 0 AND boleto != "null"');
-					$pdo_verifica->execute();
-					
-					while ($fetch = $pdo_verifica->fetch()) {
-						$data2 = $fetch['data'];
-						$data2 = implode("/", array_reverse(explode("-", $data2)));
-						$data1 = date ('d/m/Y');
-						// Comparando as Datas
-						if (strtotime($data1) > strtotime($data2)) {
-							echo '<div class="alert alert-info" role="alert"><strong>Atenção!</strong> Seu boleto está disponível para pagamento, e vence no dia '.$data2.'.</div>';
-						}
-						if (strtotime($data1) == strtotime($data2)) {
-							echo '<div class="alert alert-warning" role="alert"><strong>Atenção!</strong> Seu boleto vence hoje '.$data2.'.</div>';
-						}
-						if (strtotime($data1) < strtotime($data2)) {
-							echo '<div class="alert alert-danger" role="alert"><strong>Atenção!</strong> Seu boleto venceu em '.$data2.', entre em contato com o desenvolvedor.</div>';
-						}
-					}
-				?>
+				<?php echo $avisoPagamentoLogin ?>
 				<form action="../" method="post">
 					<?php if (!empty($_SESSION['login_erro'])) :
 						echo "<div class='alert alert-danger'>" . $_SESSION['login_erro'] . "</div>";
---
+++ src/varialvel-aviso.php
@@ -0,0 +1,23 @@
+<?php
+include('./login/config.php');
+    $avisoPagamentoLogin = (
+        $pdo_verifica = $conexao_pdo->prepare('SELECT * FROM pagamentos WHERE status = 0 AND boleto != "null"');
+        $pdo_verifica->execute();
+        
+        while ($fetch = $pdo_verifica->fetch()) {
+            $data2 = $fetch['data'];
+            $data2 = implode("/", array_reverse(explode("-", $data2)));
+            $data1 = date ('d/m/Y');
+            // Comparando as Datas
+            if (strtotime($data1) > strtotime($data2)) {
+                echo '<div class="alert alert-info" role="alert"><strong>Atenção!</strong> Seu boleto está disponível para pagamento, e vence no dia '.$data2.'.</div>';
+            }
+            if (strtotime($data1) == strtotime($data2)) {
+                echo '<div class="alert alert-warning" role="alert"><strong>Atenção!</strong> Seu boleto vence hoje '.$data2.'.</div>';
+            }
+            if (strtotime($data1) < strtotime($data2)) {
+                echo '<div class="alert alert-danger" role="alert"><strong>Atenção!</strong> Seu boleto venceu em '.$data2.', entre em contato com o desenvolvedor.</div>';
+            }
+        }
+    );
+?>
