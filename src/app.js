d50363f3ad495ac965c4c2f1b4e6160dccfb9725
RayanDiniz <rayancassiokiol@gmail.com>
2021-04-02 16:13:51 -0300

    atualização

--- src/app.js
+++ src/app.js
@@ -18,3 +18,22 @@ $("#menu-toggle").click(function(e) {
    e.preventDefault();
    $("#wrapper").toggleClass("toggled");
 });*/
+
+$('#myCarousel').carousel({
+   interval: 10000
+ })
+ 
+ $('.carousel .item').each(function(){
+   var next = $(this).next();
+   if (!next.length) {
+     next = $(this).siblings(':first');
+   }
+   next.children(':first-child').clone().appendTo($(this));
+   
+   if (next.next().length>0) {
+     next.next().children(':first-child').clone().appendTo($(this));
+   }
+   else {
+       $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+   }
+ });
